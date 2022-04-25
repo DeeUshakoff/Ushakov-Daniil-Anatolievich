6270648959cac837363f949974c207b3e03a01c4
Dee Ush <dee.ushakoff@outlook.com>
2022-04-25 19:28:58 +0300

    .

---
+++ Study/Homework/AVLTree.cs
@@ -0,0 +1,11 @@
+﻿namespace Programming.Homework;
+
+public class AVLTree<T> : BinarySearchTree<T> where T : IComparable, IComparable<T>
+{
+    public new void Add(TreeNode<T> node)
+    {
+        base.Add(node);
+        
+    }
+    
+}
